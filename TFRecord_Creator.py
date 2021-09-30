a305d1f03d7ac7773c805fad1ac437913faf6b98
AlexZhurkevich <36546909+AlexZhurkevich@users.noreply.github.com>
2021-09-30 11:41:52 -0400

    Add files via upload

--- TFRecord_Creator.py
+++ TFRecord_Creator.py
@@ -1,19 +1,3 @@
-#
-# Copyright (c) 2021, Alexander Zhurkevich. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 from glob import glob
 import tensorflow as tf
 import argparse
@@ -119,7 +103,6 @@ def tfrecord_creation(dataset, tfrecord_label, tile_size, outdir):
         else:
             #Basecase
             if im_counter == 0:
-                print("First iteration")
                 total_counter = 0
                 output_file  = os.path.join(outdir, '%d_%s_%s_.tfrecord' % (tile_size, tfrecord_label, org_file_name))
                 writer = tf.io.TFRecordWriter(output_file)
